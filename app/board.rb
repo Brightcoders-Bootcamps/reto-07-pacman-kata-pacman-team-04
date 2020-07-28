6bf6fbe06962ddb58a1820b5777170c9f0cbef2d
PaoSantollo <44455124+PaoSantollo@users.noreply.github.com>
2020-07-28 18:38:34 -0500

    Creation of the board
    Co-Authored-By: Diego Figueroa <45472379+diegofigueroa98@users.noreply.github.com>

--- app/board.rb
+++ app/board.rb
@@ -5,4 +5,18 @@ class Board
   def initialize
     
   end
+
+  def read_file
+    nom_archivo ="../board/board1.txt"
+    open(nom_archivo,"r").each do |line|
+      puts "#{line}"
+      print a
+    end
+  end
+
+
 end
+
+board= Board.new
+board.read_file
+#************************ . < v > ^   · ■ ¤ ৹ ৹૦    ૰ഠ ºဝ၀ߋ𐒆𐤏ꔷ ●·
